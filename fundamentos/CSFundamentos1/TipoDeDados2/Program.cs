0f7a4f30ea3dd4498079f1d0867e70de7bd008b1
AndreCandidoDev <andd68@hotmail.com>
2024-12-05 17:58:25 -0300

    float double e decimal

---
+++ fundamentos/CSFundamentos1/TipoDeDados2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Valores float, double e decimal");
+Console.WriteLine();
+
+double n1 = 1.234;
+float n2 = 1.345f;
+decimal n3 = 14567m;
+
+decimal n4;
+n4 = 1.678m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+Console.WriteLine(n4);
+
+Console.ReadLine();
+
+Console.WriteLine("Precisoes");
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadLine();
