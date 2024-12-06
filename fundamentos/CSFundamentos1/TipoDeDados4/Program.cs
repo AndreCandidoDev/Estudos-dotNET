30f5f201e911e4f9e8001bcd18226172d4dcfea3
AndreCandidoDev <andd68@hotmail.com>
2024-12-06 17:33:28 -0300

    string object e dynamic

---
+++ fundamentos/CSFundamentos1/TipoDeDados4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("String Object e Dynamic");
+Console.WriteLine();
+
+// strings sao imutaveis, cada alteração aloca mais um valor na memoria
+string nome = "Alguma coisa";
+String titulo = "Algo";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+dynamic nota = 10;
+dynamic valor1 = 8.55m;
+dynamic nome1 = "coisa";
+object ativo = true;
+object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativo);
+Console.WriteLine(letra);
+
+Console.ReadLine();
